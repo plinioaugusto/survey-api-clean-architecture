7fcc408652a3b869d9900396f1fbb4ec0c119653
Plínio Augusto <plinioaugustoadm@hotmail.com>
2022-08-01 00:35:13 -0300

    feat: :sparkles: ensure SignUpController return 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -22,9 +22,9 @@ const makeAddAccount = (): AddAccount => {
     add (account: AddAccountModel): AccountModel {
       const fakeAccount = {
         id: 'valid_id',
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password'
+        name: 'Plínio Augusto',
+        email: 'plinio@dev.com.br',
+        password: '123456'
       }
       return fakeAccount
     }
@@ -211,5 +211,27 @@ describe('Signup Controller', () => {
 
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'Plínio Augusto',
+        email: 'plinio@dev.com.br',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'Plínio Augusto',
+      email: 'plinio@dev.com.br',
+      password: '123456'
+    })
   })
 })
