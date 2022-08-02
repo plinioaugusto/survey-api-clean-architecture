12d9a2dfbfd36ded45e0c83777cf9e2b40f85ad6
Plínio Augusto <plinioaugustoadm@hotmail.com>
2022-08-01 23:24:50 -0300

    test: :white_check_mark: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email.validator.adapter.spec.ts
+++ src/utils/email.validator.adapter.spec.ts
@@ -14,9 +14,17 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
+
   test('Should return true if validator return true', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('valid_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@mail.com')
   })
 })
